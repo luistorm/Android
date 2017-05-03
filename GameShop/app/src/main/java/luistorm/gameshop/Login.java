4063a49341843f0be93d44a120ac890357e9ac5c
LuiStorm <luistorresm0905@gmail.com>
2017-05-02 21:37:06 -0400

    Example of a simple virtual shop

---
+++ GameShop/app/src/main/java/luistorm/gameshop/Login.java
@@ -0,0 +1,15 @@
+package luistorm.gameshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Esta aplicacion fue hecha con fines educativos y para compartirla, si sientes que violo
+ * algun derecho de autor o algo parecido escribeme: luistorresm0905@gmail.com*/
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
